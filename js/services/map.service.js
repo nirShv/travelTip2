0b444edc331a4a552c699afefd0bb24c9b712c31
risanBenichou <ris.benichou@gmail.com>
2022-08-17 15:57:23 +0300

    added utills,click events, storage, mapAPI,

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -4,9 +4,13 @@ export const mapService = {
     panTo
 }
 
+import { utilsService } from './utils.js'
+import { storageService } from './storage.service.js'
+
 
 // Var that is used throughout this Module (not global)
 var gMap
+var gPlaces = []
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
     console.log('InitMap')
@@ -19,6 +23,24 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
                 zoom: 15
             })
             console.log('Map!', gMap)
+
+            gMap.addListener('click', (mapsMouseEvent) => {
+                const lat = mapsMouseEvent.latLng.lat()
+                const lng = mapsMouseEvent.latLng.lng()
+                const position = { lat, lng }
+            
+                const locationName = prompt('Enter location name')
+                if (locationName) {
+                  onAddPlace(position, locationName)
+            
+                  new google.maps.Marker({
+                    position: position,
+                    map: gMap,
+                  })
+                }
+              })
+
+
         })
 }
 
@@ -39,7 +61,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyAumIxeXD96iDEVcmHW74JGeIxq0ryRg8Y'
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
@@ -50,3 +72,13 @@ function _connectGoogleApi() {
         elGoogleApi.onerror = () => reject('Google script failed to load')
     })
 }
+
+function onAddPlace(pos, name) {
+    addPlace(pos, name)
+    // renderPlaces()
+  }
+
+function addPlace(pos, name) {
+    gPlaces.push({ id:utilsService.makeId(3), pos, name })
+    storageService.save('places', gPlaces)
+  }
---
+++ js/services/utils.js
@@ -0,0 +1,14 @@
+export const utilsService = {
+    makeId
+}
+
+function makeId(length) {
+    var text = ''
+    var possible = '0123456789'
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+  
+    return text
+  }
